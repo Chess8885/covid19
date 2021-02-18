138d60a34e17ea14d849309fcb61b840b84e3a66
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-02-18 19:14:36 +0100

    [ADD] skeleton header

--- covid19-tracker/src/App.js
+++ covid19-tracker/src/App.js
@@ -1,9 +1,33 @@
+import React from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a covid 19 tracker</h1>
+      <div className="app__header">
+      <h1>Covid19 Tracker</h1>
+
+
+      
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          value="abc"
+        >
+
+      <MenuItem value="worldwide">Worldwide</MenuItem>
+      <MenuItem value="worldwide">Option 2</MenuItem>
+      <MenuItem value="worldwide">Option 3</MenuItem>
+      <MenuItem value="worldwide">Option 4</MenuItem>
+      </Select> 
+
+      </FormControl>
+      </div>
 
       {/* Header */}
       {/* Title */}
