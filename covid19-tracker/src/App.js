d46434ecbc255db19ab589e1c017551c85a559b5
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-02-23 05:22:25 +0100

    [ADD] display all countries and list them by nb cases

--- covid19-tracker/src/App.js
+++ covid19-tracker/src/App.js
@@ -9,11 +9,14 @@ import {
 import './App.css';
 import InfoBox from './InfoBox';
 import Map from './Map';
+import Table from './Table';
+import { sortData } from './util';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide')
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
 useEffect(() => {
   fetch("https://disease.sh/v3/covid-19/all")
@@ -33,9 +36,12 @@ useEffect(() => {
               name: country.country,
               value: country.countryInfo.iso2 // us, uk, fr
             }));
+            const sortedData = sortData(data);
+            setTableData(sortedData);
             setCountries(countries);
         });
     };
+    
     getCountriesData();
   }, []);
 
@@ -86,6 +92,7 @@ useEffect(() => {
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
         </CardContent>
       {/* Table */}
